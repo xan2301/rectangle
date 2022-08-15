4c2cda4b5aed98d9ab1951a7043ba9f22f37bb87
xan2301 <2silverghost2@gmail.com>
2022-08-15 15:30:13 +0200

    commit 1

---
+++ src/main/java/pl/wiktorowski/rectangleapp/RectangleappApplication.java
@@ -0,0 +1,13 @@
+package pl.wiktorowski.rectangleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RectangleappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RectangleappApplication.class, args);
+    }
+
+}
