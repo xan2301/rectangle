47a4fe91289fef0784b58f527aecb2201644c101
xan2301 <2silverghost2@gmail.com>
2022-08-18 12:55:27 +0200

    1.0.1
    (51,26)

--- src/main/java/pl/wiktorowski/rectangleapp/RectangleReader.java
+++ src/main/java/pl/wiktorowski/rectangleapp/RectangleReader.java
@@ -1,13 +1,24 @@
 package pl.wiktorowski.rectangleapp;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Service
 
 public class RectangleReader {
+
+    private RectangleRepo rectangleRepo;
+
+    @Autowired
+
+    public RectangleReader(RectangleRepo rectangleRepo) {
+        this.rectangleRepo = rectangleRepo;
+    }
 
     @Value("${siteA1}")
     private int siteA1;
@@ -34,13 +45,19 @@ public class RectangleReader {
     private int siteB4;
 
 
-    @GetMapping("/add")
+    @EventListener(ApplicationReadyEvent.class)
 
     public void addRectangles() {
         Rectangle rectangle1 = new Rectangle(siteA1, siteB1);
         Rectangle rectangle2 = new Rectangle(siteA2, siteB2);
         Rectangle rectangle3 = new Rectangle(siteA3, siteB3);
         Rectangle rectangle4 = new Rectangle(siteA4, siteB4);
+
+        rectangleRepo.save(rectangle1);
+        rectangleRepo.save(rectangle2);
+        rectangleRepo.save(rectangle3);
+        rectangleRepo.save(rectangle4);
+
 
     }
 
