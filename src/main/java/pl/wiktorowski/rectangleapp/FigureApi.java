4eac48b470af1c54a8fc5e8c532c4c47351addd9
xan2301 <2silverghost2@gmail.com>
2022-08-18 11:11:15 +0200

    1.0.1
    (40.43)

--- src/main/java/pl/wiktorowski/rectangleapp/FigureApi.java
+++ src/main/java/pl/wiktorowski/rectangleapp/FigureApi.java
@@ -8,10 +8,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FigureApi {
 
-@GetMapping("/calculate")
-    public int calculate (@RequestParam String figure, @RequestParam int siteA, @RequestParam int siteB ){
+    @GetMapping("/calculate")
+    public int calculate(@RequestParam String figure, @RequestParam int siteA, @RequestParam int siteB, @RequestParam(required = false) Integer siteC) {
+        if (figure.equalsIgnoreCase("RECTANGLE")) {
+
+            return 2 * siteA + 2 * siteB;
+
+        } else if (figure.equalsIgnoreCase("TRIANGLE")) {
+
+            return siteA + siteB + siteC;
 
 
+        }
 
+        return -1;
+    }
 
-}}
+}
